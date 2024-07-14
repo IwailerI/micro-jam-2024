eb10507b5ac65ada4e378a308c028d2dadb52df5
Andrey Pirozhenok <andrypirk@gmail.com>
2024-07-14 11:31:40 +0300

    Touched up main menu

--- src/scenes/main_menu/play_button.gd
+++ src/scenes/main_menu/play_button.gd
@@ -1,4 +1,6 @@
 extends TextureButton
 
+const MAIN_SCENE := preload ("res://src/scenes/field/field.tscn")
+
 func _pressed() -> void:
-	get_tree().change_scene_to_file("res://src/scenes/field/field.tscn")
+	get_tree().change_scene_to_packed(MAIN_SCENE)
