dd6dcf9a989f45839ed0b0789017099f17786638
Роман Труханов <vadim.truhanov@inbox.lv>
2024-07-14 15:10:59 +0300

    Added music in the game

--- src/scenes/field/music_node.gd
+++ src/scenes/field/music_node.gd
@@ -1,15 +1,19 @@
 extends Node
 
-func _ready():
-	$Intro.finished.connect(func() -> void:
-		$Loop.play()
-		$Drums.play())
-	$Loop.finished.connect(func() -> void:
-		$Loop.play()
-		$Drums.play())
+@onready var IntroPlayer: AudioStreamPlayer = %Intro
+@onready var DrumsPlayer: AudioStreamPlayer = %Drums
+@onready var LoopPlayer: AudioStreamPlayer = %Loop
+
+func _ready() -> void:
+	IntroPlayer.finished.connect(func() -> void:
+		LoopPlayer.play()
+		DrumsPlayer.play())
+	LoopPlayer.finished.connect(func() -> void:
+		LoopPlayer.play()
+		DrumsPlayer.play())
 
 func _physics_process(_delta: float) -> void:
 	if get_tree().get_nodes_in_group("Enemy").is_empty():
-		$Drums.volume_db = -100
+		DrumsPlayer.volume_db = -100
 	else:
-		$Drums.volume_db = 0
+		DrumsPlayer.volume_db = 0
